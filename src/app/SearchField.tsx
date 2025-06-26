2ac67226afc3bc99592fc293e4a7db5b9584614d
David McGrath <github.com@davidmcgrath.com>
2025-06-26 11:31:36 -0400

    style SearchField

--- src/app/SearchField.tsx
+++ src/app/SearchField.tsx
@@ -1,4 +1,28 @@
 import { ChangeEventHandler } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: relative;
+
+  max-width: 250px;
+`
+
+const Input = styled.input`
+  width: 100%;
+
+  border: 1px solid black;
+  border-radius: 3px;
+  padding: 6px;
+  padding-right: 22px;
+`
+
+const ResetButton = styled.button`
+  position: absolute;
+  top: 6px;
+  bottom: 6px;
+  right: 6px;
+  font-weight: 700;
+`
 
 type Props = {
   onChange: (newValue: string) => void;
@@ -18,10 +42,18 @@ const SearchField = ({onChange, value}: Props) => {
 
 
   return (
-    <div>
-      <input style={{ border: "1px solid black" }} onChange={handleChange} value={value} />
-      <button onClick={handleReset}>Reset Search</button>
-    </div>
+    <Container>
+      <Input
+        onChange={handleChange}
+        placeholder="Describe what you need"
+        value={value}
+      />
+      {
+        value && (
+          <ResetButton onClick={handleReset}>×</ResetButton>
+        )
+      }
+    </Container>
   )
 };
 
