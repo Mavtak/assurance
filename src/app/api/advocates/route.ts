174ade43a94a9c67dc3279479646dc36439f85d3
David McGrath <github.com@davidmcgrath.com>
2025-06-26 10:24:18 -0400

    support searchTerm in API route

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,8 +1,22 @@
+import { ilike, or } from "drizzle-orm";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 
-export async function GET() {
-  const data = await db.select().from(advocates);
+export async function GET(request: Request) {
+  const queryParams = new URL(request.url).searchParams;
+  const searchTerm = queryParams.get('searchTerm') || '';
+
+  const data = await db
+    .select()
+    .from(advocates)
+    .where(
+      or(
+        ilike(advocates.firstName, `%${searchTerm}%`),
+        ilike(advocates.lastName, `%${searchTerm}%`),
+        ilike(advocates.city, `%${searchTerm}%`),
+        ilike(advocates.degree, `%${searchTerm}%`),
+      )
+    );
 
   return Response.json({ data });
 }
