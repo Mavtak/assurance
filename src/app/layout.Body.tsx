6e2a9bada9b5797934ff54fe0ed4a3cefdaf1979
David McGrath <github.com@davidmcgrath.com>
2025-06-26 11:11:07 -0400

    add basic style for layout

--- src/app/layout.Body.tsx
+++ src/app/layout.Body.tsx
@@ -1,5 +1,28 @@
+"use client"
+
 import { Inter } from "next/font/google";
 import { ReactNode } from "react";
+import styled from "styled-components";
+
+const StyledBody = styled.body`
+  margin-left: auto;
+  margin-right: auto;
+
+  max-width: 1000px;
+  padding: 25px;
+
+  background-color: #265b4e;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  border-radius: 15px;
+  padding: 15px;
+  background-color: white;
+`
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,7 +32,11 @@ type Props = {
 
 const Body = ({children}: Props) => {
   return (
-    <body className={inter.className}>{children}</body>
+    <StyledBody className={inter.className}>
+      <Content>
+        {children}
+      </Content>
+    </StyledBody>
   )
 }
 
